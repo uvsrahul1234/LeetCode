075ac6fdc6c7e9de91fe900f317a33cd212bcbba
Rahul <uvsrahul@gmail.com>
2025-03-23 23:20:26 -0500

    movedToSql

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-
-SELECT
-    p.product_name,
-    s.year,
-    s.price
-FROM sales as s
-LEFT JOIN product as p
-ON s.product_id = p.product_id
--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-
-SELECT
-    eu.unique_id,
-    e.name
-FROM employees AS e
-LEFT JOIN employeeuni AS eu
-ON e.id = eu.id
-
