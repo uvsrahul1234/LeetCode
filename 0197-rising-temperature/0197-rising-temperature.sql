8d56406bd69676e9c2ad0357a7eda22ac585dfef
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2025-03-26 13:13:25 -0500

    0197-rising-temperature Easy

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,7 +3,7 @@ SELECT
 FROM weather AS w1
 JOIN weather AS w2
 ON DATEDIFF(w1.recordDate, w2.recordDate) = -1
-AND w2.temperature > w1.temperature
+WHERE w2.temperature > w1.temperature
 
 -- SELECT w1.id 
 -- FROM Weather w1, Weather w2
