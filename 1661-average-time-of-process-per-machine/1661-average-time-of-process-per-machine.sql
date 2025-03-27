cb80a1b9b403345ebeb7c8c281fb7569a3c2a99d
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2025-03-27 03:26:05 -0500

    1661-average-time-of-process-per-machine Easy

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,11 +1,20 @@
 # Write your MySQL query statement below
 
-SELECT
-    a1.machine_id,
-    ROUND(AVG((a2.timestamp - a1.timestamp)),3) AS processing_time
-FROM activity AS a1
-JOIN activity AS a2
-ON a1.machine_id = a2.machine_id
-AND a1.process_id = a2.process_id
-AND a1.timestamp < a2.timestamp
-GROUP BY a1.machine_id
+-- SELECT
+--     a1.machine_id,
+--     ROUND(AVG((a2.timestamp - a1.timestamp)),3) AS processing_time
+-- FROM activity AS a1
+-- JOIN activity AS a2
+-- ON a1.machine_id = a2.machine_id
+-- AND a1.process_id = a2.process_id
+-- AND a1.timestamp < a2.timestamp
+-- GROUP BY a1.machine_id
+
+select a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3)
+as processing_time
+from activity a1 join activity a2
+on a1.machine_id = a2.machine_id
+and a1.process_id = a2.process_id
+and a1.activity_type = 'start'
+and a2.activity_type = 'end'
+group by a1.machine_id
