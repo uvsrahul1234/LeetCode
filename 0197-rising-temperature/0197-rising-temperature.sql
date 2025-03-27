e0b266977fba58d8737b9a7ed21d8b1c06db607f
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2025-03-26 22:01:32 -0500

    0197-rising-temperature Easy

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -9,4 +9,4 @@ WHERE w2.temperature > w1.temperature
 -- SELECT w1.id 
 -- FROM Weather w1, Weather w2
 -- WHERE w1.recordDate = w2.recordDate + INTERVAL 1 DAY
--- AND w1.temperature > w2.temperature;
+-- AND w1.temperature > w2.temperature
