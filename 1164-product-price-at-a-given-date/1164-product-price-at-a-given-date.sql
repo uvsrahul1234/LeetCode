87bced72936ebb806be41be324ac6860699585e4
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-01-23 23:51:59 -0600

    Time: 893 ms (13.39%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+
+SELECT
+    product_id,
+    new_price as price
+FROM Products
+WHERE (product_id, change_date) IN
+(SELECT
+    product_id,
+    MAX(change_date)
+FROM Products
+WHERE change_date <= '2019-08-16'
+GROUP BY product_id
+)
+UNION
+SELECT
+    DISTINCT product_id,
+    10 as price
+FROM Products
+WHERE product_id NOT IN
+(SELECT
+    product_id
+FROM Products
+WHERE change_date <= '2019-08-16'
+)
