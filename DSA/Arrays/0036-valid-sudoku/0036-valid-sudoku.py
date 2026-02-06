2f17ee4160073b2afee16042d1e30cd1f6dd266d
Rahul <uvsrahul1234@gmail.com>
2026-02-05 18:10:16 -0600

    Time: 7 ms (33.39%), Space: 19.4 MB (40.66%)

--- DSA/Arrays/0036-valid-sudoku/0036-valid-sudoku.py
+++ DSA/Arrays/0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,5 +1,6 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
         rows = defaultdict(set)
         cols = defaultdict(set)
         squares = defaultdict(set) # Key: (r // 3, c // 3)
@@ -21,3 +22,38 @@ class Solution:
                 squares[(r // 3, c // 3)].add(board[r][c])
                 
         return True
+
+        # Brute Force T:(N2) S:O(N)
+    #     for i in board:
+    #         count = defaultdict(int)
+    #         for j in i:
+    #             if j != '.':
+    #                 count[j] += 1
+    #                 if count[j] > 1:
+    #                     return False
+        
+    #     for i in range(9):
+    #         count = defaultdict(int)
+    #         for j in range(9):
+    #             if board[j][i] != '.':
+    #                 count[board[j][i]] += 1
+    #                 if count[board[j][i]] > 1:
+    #                     return False
+        
+    #     if self.isValidBox(board, 0, 3, 0, 3) and self.isValidBox(board, 0, 3, 3, 6) and self.isValidBox(board, 0, 3, 6, 9) and self.isValidBox(board, 3, 6, 0, 3) and self.isValidBox(board, 3, 6, 3, 6) and self.isValidBox(board, 3, 6, 6, 9) and self.isValidBox(board, 6, 9, 0, 3) and self.isValidBox(board, 6, 9, 3, 6) and self.isValidBox(board, 6, 9, 6, 9):
+    #         return True
+    #     else:
+    #         return False
+        
+        
+    #     return True
+
+    # def isValidBox(self, board, tr, br, lc, rc) -> bool:
+    #     count = defaultdict(int)
+    #     for i in range(tr, br):
+    #         for j in range(lc, rc):
+    #             if board[i][j] != '.':
+    #                 count[board[i][j]] += 1
+    #                 if count[board[i][j]] > 1:
+    #                     return False
+    #     return True
