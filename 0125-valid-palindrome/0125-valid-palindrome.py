267deb1452ff9a002d06fbb76494a1a82d865f6b
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-02-07 11:40:25 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,29 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # T:O(N) S:O(N)
-        newStr = ""
+        # Two Pointers T:O(N) S:O(1)
+        l = 0
+        r = len(s) - 1
 
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
+        while l < r:
+            while not self.alphaNumeric(s[l]):
+                l += 1
+            while not self.alphaNumeric(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        
+        return True
+    
+    def alphaNumeric(self, ch):
+        return (ord('0') <= ord(ch) <= ord('9') or
+                ord('A') <= ord(ch) <= ord('Z') or
+                ord('a') <= ord(ch) <= ord('z'))
+        
+        # T:O(N) S:O(N) Using inbuilt functionality
+        # newStr = ""
+
+        # for c in s:
+        #     if c.isalnum():
+        #         newStr += c.lower()
+        # return newStr == newStr[::-1]
