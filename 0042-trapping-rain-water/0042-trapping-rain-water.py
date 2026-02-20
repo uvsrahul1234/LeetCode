12a25691576740e191ba3a853a70b51faf2a3f1b
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-02-20 17:57:15 -0600

    Time: 7 ms (78.13%), Space: 21.2 MB (37.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,12 +1,11 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-    
+        
+        # Two pointers: T:O(N) S:O(1)
         l, r = 0, len(height) - 1
         leftMax, rightMax = height[l], height[r]
         res = 0
-        
+
         while l < r:
             if leftMax < rightMax:
                 l += 1
@@ -16,5 +15,32 @@ class Solution:
                 r -= 1
                 rightMax = max(rightMax, height[r])
                 res += rightMax - height[r]
-                
         return res
+
+        # Brute Force: T:O(N) S:O(N)
+        # maxleft = [0] * len(height)
+        # maxright = [0] * len(height)
+
+        # # getting max height on left for each height
+        # for i in range(0, len(height)):
+        #     if i == 0:
+        #         maxleft[i] = 0
+        #     else:
+        #         maxleft[i] = max(height[i - 1], maxleft[i - 1])
+        
+        # # getting max height on right for each height
+        # for i in range(len(height) - 1, -1, -1):
+        #     if i == len(height) - 1:
+        #         maxright[i] = 0
+        #     else:
+        #         maxright[i] = max(height[i + 1], maxright[i + 1])
+        
+        # minarr = [0] * len(height)
+        
+        # res = 0
+        # for i in range(0, len(height)):
+        #     minarr[i] = min(maxleft[i], maxright[i])
+        #     if (minarr[i] - height[i]) > 0:
+        #         res += minarr[i] - height[i]
+
+        # return res
