9755a5508f63aad263c982ad86eec8d0ce986eed
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-03-06 19:35:42 -0600

    Time: 8 ms (33.63%), Space: 24.8 MB (18.65%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # HashMap Buckt Sort T:O(N) S:O(N)
+        count = defaultdict(int)
+
+        for i in nums:
+            count[i] = 1 + count[i]
+        
+        freq = [[] for _ in range(len(nums) + 1)]
+
+        for n, c in count.items():
+            freq[c].append(n)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
