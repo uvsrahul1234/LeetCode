ab9ac50100277e6fd260fce5c79989cce78b6213
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-03-13 22:59:01 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return false
+        
+        count = defaultdict(int)
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for v, k in count.items():
+            if k != 0:
+                return False
+        
+        return True
