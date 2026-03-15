0884caaeea7edd89f7f3d603fa59ae792dc7b5de
Rahul <uvsrahul1234@gmail.com>
2026-03-14 21:58:18 -0500

    Time: 0 ms (100.00%), Space: 20.56 MB (38.75%)

--- DSA/Arrays/0001-two-sum/0001-two-sum.py
+++ DSA/Arrays/0001-two-sum/0001-two-sum.py
@@ -1,7 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Brute force
+        
+        # Hashmap
+        count = {}
         for i, n in enumerate(nums):
-            for j in range(i+1, len(nums)):
-                if n + nums[j] == target:
-                    return [i, j]
+            diff = target - n
+            if diff in count:
+                return [count[diff] ,i]
+            count[n] = i
+        
+        # Brute force
+        # for i, n in enumerate(nums):
+        #     for j in range(i+1, len(nums)):
+        #         if n + nums[j] == target:
+        #             return [i, j]
