dd399d368d63016f6b8daa097969ede9e1df11a9
Rahul <uvsrahul1234@gmail.com>
2026-03-15 01:23:51 -0500

    Time: 8 ms (32.91%), Space: 24.82 MB (17.70%)

--- DSA/Arrays/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ DSA/Arrays/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        # Hashmap Bucket sort O(N) O(N)
         count = defaultdict(int)
-
+        res = [ [] for _ in range(len(nums))]
         for i in nums:
-            count[i] = 1 + count[i]
+            count[i] += 1
         
-        freq = [[] for _ in range(len(nums) + 1)]
+        for r, v in count.items():
+            res[v - 1].append(r)
+        
+        print(res)
+        ans = []        
+        for i in range(len(res) - 1, -1, -1):
+            for n in res[i]:
+                ans.append(n)
+                if len(ans) == k:
+                    return ans
 
-        for n, c in count.items():
-            freq[c].append(n)
-        
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+
+
