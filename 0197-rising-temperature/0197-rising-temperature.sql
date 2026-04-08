695ec658194bd56089314f4c86f198003bc64ad6
Rahul <uvsrahul1234@gmail.com>
2026-04-07 22:01:08 -0500

    completed

--- 0197-rising-temperature/0197-rising-temperature.sql
+++
@@ -1,7 +0,0 @@
-# Write your MySQL query statement below
-SELECT
-    w1.id
-FROM Weather as w
-LEFT JOIN Weather as w1
-ON DATEDIFF(w.recordDate, w1.recordDate) = -1
-WHERE w1.temperature > w.temperature
