a8639cb9da9fb1832633fece5e999846a1af4b2a
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-05-29 07:21:38 +0530

    Refactor trap function to calculate profit

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,13 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
-        lmax, rmax = height[l], height[r] 
-        vol = 0
+       l, r = 0, 1
+        profit = 0
         
-        while l < r:
-            if lmax < rmax:
-                l += 1
-                lmax = max(lmax, height[l])
-                vol += lmax - height[l]
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = max(profit, prices[r] - prices[l])
             else:
-                r -= 1
-                rmax = max(rmax, height[r])
-                vol += rmax - height[r]
-            
-        return vol
+                l = r
+            r += 1
+        
+        return profit
