1d4ba700080713fee8c050fe926f052b23ee786f
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-05-30 03:47:05 +0530

    Replace containsDuplicate with minWindow method

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,32 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        res = set()
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "" or len(s) < len(t):
+            return ""
 
-        for n in nums:
-            if n in res:
-                return True
-            res.add(n)
+        countT, countS = {}, {}
+        for i in t:
+            countT[i] = countT.get(i, 0) + 1
         
-        return False
+        have, need = 0, len(countT)
+        res, reslen = [-1, -1], float("infinity")
+
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+
+            countS[c] = countS.get(c, 0) + 1
+            if c in countT and countT[c] == countS[c]:
+                have += 1
+            
+            while have == need:
+                if (r - l + 1) < reslen:
+                    res = [l, r]
+                    reslen = r - l + 1
+
+                countS[s[l]] -= 1
+                if s[l] in countT and countS[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+
+        l, r = res
+        return s[l: r + 1]
