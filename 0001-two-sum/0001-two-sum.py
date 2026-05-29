3bb92dd1826df4f7fe5f39e93fda31d9cd7d711c
uvsrahul1234 <52215587+uvsrahul1234@users.noreply.github.com>
2026-05-29 07:58:05 +0530

    Add lengthOfLongestSubstring method to Solution class
    Implement lengthOfLongestSubstring method to find the length of the longest substring without repeating characters.

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,12 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
-       l, r = 0, 1
-        profit = 0
+    def lengthOfLongestSubstring(self, s: str) -> int:
         
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = max(profit, prices[r] - prices[l])
-            else:
-                l = r
-            r += 1
-        
-        return profit
+        l, r = 0, 0
+        res = set()
+        count = 0
+        while r < len(s):
+            while s[r] in res:
+                res.remove(s[l])
+                l += 1
+            
+            res.add(s[r])
